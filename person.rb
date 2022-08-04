a3d4b1f9968b7864ccfa286ed0b3760e24b8d193
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-08-05 01:22:42 +0300

    Add relationships

--- person.rb
+++ person.rb
@@ -1,15 +1,17 @@
 require_relative './nameable'
+require './rental'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, rental=[])
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     super()
+    @rentals = rentals
   end
 
   def can_use_services?
@@ -18,6 +20,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 
   private
